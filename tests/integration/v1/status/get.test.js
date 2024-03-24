ba8a0370967d31a5bd2e2d3a58edd0e9187ce65d
pedrolvidotto <pedrol.vidotto@gmail.com>
2024-03-24 18:45:28 -0300

    chore: .env

--- tests/integration/v1/status/get.test.js
+++ tests/integration/v1/status/get.test.js
@@ -1,4 +1,7 @@
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("tests status", () => {
+  test("GET to /api/v1/status should return 200", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    expect(response.status).toBe(200);
+    console.log("teste");
+  });
 });
