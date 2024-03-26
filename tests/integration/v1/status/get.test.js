d20636d633def0ab56f764e578b85540b769b45e
pedrolvidotto <pedrol.vidotto@gmail.com>
2024-03-25 22:33:23 -0300

    add max_connections and opnned_connections in status

--- tests/integration/v1/status/get.test.js
+++ tests/integration/v1/status/get.test.js
@@ -2,6 +2,13 @@ describe("tests status", () => {
   test("GET to /api/v1/status should return 200", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status");
     expect(response.status).toBe(200);
-    console.log("teste");
+
+    const responseBody = await response.json();
+    const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+
+    expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+    expect(responseBody.dependencies.database.version).toEqual("16.0");
+    expect(responseBody.dependencies.database.max_connections).toEqual(100);
+    expect(responseBody.dependencies.database.openned_connections).toEqual(1);
   });
 });
