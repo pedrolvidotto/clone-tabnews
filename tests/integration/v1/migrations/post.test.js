e092c632285f6a26ff3bec95ec6c0f9f21e4439c
pedrolvidotto <pedrol.vidotto@gmail.com>
2024-05-04 14:49:19 -0300

    ajuste espacamento

--- tests/integration/v1/migrations/post.test.js
+++ tests/integration/v1/migrations/post.test.js
@@ -10,6 +10,7 @@ test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
+
   expect(response.status).toBe(201);
   const responseBody = await response.json();
   expect(Array.isArray(responseBody)).toBe(true);
@@ -18,6 +19,7 @@ test("GET to /api/v1/migrations should return 200", async () => {
   const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
+
   expect(response2.status).toBe(200);
   const response2Body = await response2.json();
   expect(Array.isArray(response2Body)).toBe(true);
