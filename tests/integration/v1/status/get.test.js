1f921f7e705a81fb7a21c457be1cd3ee6ee466a0
pedrolvidotto <pedrol.vidotto@gmail.com>
2024-06-23 19:03:34 -0300

    criado orchestrator.js

--- tests/integration/v1/status/get.test.js
+++ tests/integration/v1/status/get.test.js
@@ -1,14 +1,18 @@
-describe("tests status", () => {
-  test("GET to /api/v1/status should return 200", async () => {
-    const response = await fetch("http://localhost:3000/api/v1/status");
-    expect(response.status).toBe(200);
+import orchestrator from "../orchestrator";
 
-    const responseBody = await response.json();
-    const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
 
-    expect(responseBody.updated_at).toEqual(parsedUpdateAt);
-    expect(responseBody.dependencies.database.version).toEqual("16.0");
-    expect(responseBody.dependencies.database.max_connections).toEqual(100);
-    expect(responseBody.dependencies.database.openned_connections).toEqual(1);
-  });
+test("GET to /api/v1/status should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.openned_connections).toEqual(1);
 });
