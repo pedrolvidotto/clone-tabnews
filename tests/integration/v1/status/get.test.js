c7df67503fbc8c0762a6183fc6b8b3962cba4fab
pedrolvidotto <pedrol.vidotto@gmail.com>
2024-11-25 19:46:42 -0300

    test: teste 2

--- tests/integration/v1/status/get.test.js
+++ tests/integration/v1/status/get.test.js
@@ -6,7 +6,7 @@ beforeAll(async () => {
 
 describe("GET /api/v1/status", () => {
   describe("Anonymous user", () => {
-    test("Retrieving current system status teste1", async () => {
+    test("Retrieving current system status teste 2", async () => {
       const response = await fetch("http://localhost:3000/api/v1/status");
       expect(response.status).toBe(200);
 
