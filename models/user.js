22bc6b6c8961a65a738a3effa9e234d79523ef2c
pvidotto <pedro.vidotto@objective.com.br>
2025-04-09 16:40:10 -0300

    feat: create  method and

--- models/user.js
+++ models/user.js
@@ -1,5 +1,34 @@
 import database from "infra/database";
-import { ValidationError } from "infra/errors";
+import { NotFoundError, ValidationError } from "infra/errors";
+
+async function findOneByUsername(username) {
+  const userFound = await runSelectQuery(username);
+  return userFound;
+
+  async function runSelectQuery(username) {
+    const result = await database.query({
+      text: `
+            SELECT 
+              * 
+            FROM 
+              users 
+            WHERE 
+              LOWER(username) = LOWER($1)
+            LIMIT 
+              1
+            ;`,
+      values: [username],
+    });
+
+    if (result.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O username informado não foi encontrado no sistema",
+        action: "Verifique se o username está digitado corretamente",
+      });
+    }
+    return result.rows[0];
+  }
+}
 
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
@@ -63,5 +92,5 @@ async function create(userInputValues) {
   }
 }
 
-const user = { create };
+const user = { create, findOneByUsername };
 export default user;
